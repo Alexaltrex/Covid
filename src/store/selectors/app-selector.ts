7a3aa0ec64edbda7f2fe90122b0f500b00f0e1f9
Alexaltrex <alexaltrex@mail.ru>
2021-01-16 21:50:00 +0400

    delete

--- src/store/selectors/app-selector.ts
+++
@@ -1,6 +0,0 @@
-import {StateType} from "../store";
-
-export const getIsLoading = (state: StateType) => state.app.isLoading;
-export const getLanError = (state: StateType) => state.app.lanError;
-export const getLanErrorResponse = (state: StateType) => state.app.lanErrorResponse;
-export const getLang = (state: StateType) => state.app.lang;
--- src/store/selectors/summary-selector.ts
+++
@@ -1,7 +0,0 @@
-import {StateType} from "../store";
-
-export const getDate = (state: StateType) => state.summary.date;
-export const getSummaryCases = (state: StateType) => state.summary.summaryCases;
-export const getCountriesCases = (state: StateType) => state.summary.countriesCases;
-export const getCountriesList = (state: StateType) => state.summary.countriesList;
-export const getCurrentCountry = (state: StateType) => state.summary.currentCountry;
